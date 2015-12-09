45a58c9c52a9350a2c489ca1268ebc7b4c5af510
Tim Burgess <timburgess@mac.com>
2015-12-09 14:23:00 +1000

    Add Source and Libs dir. Add SwiftCSV

---
+++ Source/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ScienceKit
+//
+//  Put your code here in main.swift
+//  Xcode turns main.swift into an executable in build/Debug/
+//
+
+import Foundation
+
+print("Hello, World!")
+
