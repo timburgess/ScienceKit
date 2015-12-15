a70c60d7920b7b0ce29ebed2339135bad60ca9a4
Tim Burgess <timburgess@mac.com>
2015-12-15 11:14:04 +1000

    Create workspace and bring reworked ReadCSV project into workspace

--- ReadCSV/Source/main.swift
+++ ReadCSV/Source/main.swift
@@ -1,15 +1,42 @@
 //
-//  main.swift
-//  ScienceKit
+//  ReadCSV.swift
+//  ScienceKit example
+//  Contributed by Tim Burgess - timburgess@mac.com
 //
-//  Put your code here in main.swift
-//  Xcode turns main.swift into an executable in build/Debug/
-//
+//  Example code for reading from a CSV file and calculating
+//  mean and standard deviation of data
 
-import Foundation
-import CorePlot
+import Foundation // required for NS thingamajigs
+import Accelerate // required for Upsurge
+import Upsurge
 
-print("Hello, World! This is ScienceKit!")
 
-// Replace this with code from an example
+/**
+ * read in SST data from filepath given
+ */
+func readCSV(path: String) -> ([String]!, [Double]!) {
+  
+  print("Reading from \(path)")
+  
+  let error: NSErrorPointer = nil
+  let space = NSCharacterSet(charactersInString: " ")
+  if let csv = CSV(contentsOfFile: path, delimiter:space, encoding: NSUTF8StringEncoding, error: error) {
+    
+    let dates = csv.columns["Date"]!
+    let sstStrings = csv.columns["SST"]!
+    print("Read \(dates.count) entries")
+    
+    // transform String array to Double array using a closure
+    // a more simplified form of this would be: sstStrings.map { Double($0)! }
+    let sst = sstStrings.map({ Double($0)! })
 
+    return (dates, sst)
+  }
+  // read error ocurred so return nil
+  return (nil, nil)
+}
+
+// by default, executable runs from ./build/debug/
+let (dates, sst) = readCSV("../../Data/SST_blended5km_00.025S_130.025W_2014-11-01_2015-11-01.txt")
+let allSST = RealArray(sst)
+print(String(format: "Mean temperature value is %5.2fC with a standard deviation of %4.2fC", mean(allSST), std(allSST)))
