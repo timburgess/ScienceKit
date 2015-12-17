988f3c0338a565308b2ae7ab99b37000d09220bd
Tim Burgess <timburgess@mac.com>
2015-12-17 10:41:45 +1000

    Day of year NSDate construction example

--- DateUtils/Source/main.swift
+++ DateUtils/Source/main.swift
@@ -15,3 +15,6 @@ print("Today is day \(today.dayOfYear) in the year \(today.year)")
 
 let someDay = NSDate(year: 1985, month: 12, day: 1)
 print("someDay is day \(someDay.dayOfYear) in the year \(someDay.year)")
+
+let doy = NSDate(year: 2009, month: 1, day: 91)
+print("doy is day \(doy.dayOfYear) in the year \(doy.year)")
